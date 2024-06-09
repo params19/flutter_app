98a7f7db7e008cf895ee5273142a7ec0162eb489
params19 <paramsrivastava9@gmail.com>
2024-06-09 16:21:43 +0530

    text field number added

--- lib/currency_converter.dart
+++ lib/currency_converter.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class CurrencyConverterMaterialPage extends StatelessWidget {
-  const CurrencyConverterMaterialPage({super.key});
+   const CurrencyConverterMaterialPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    //you can use this border for the textfield
+
+    const border= OutlineInputBorder(
+      borderSide: BorderSide(
+        width: 2.0,
+        color: Colors.white,
+      ),
+      borderRadius: BorderRadius.all(Radius.circular(60)),
+    );
+    //yahan tak hai 
+
     return const Scaffold(
       backgroundColor: Colors.black,
       body: Center(
@@ -19,22 +31,30 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
                 color: Colors.white,
               ),
             ),
-            TextField(
-              style: TextStyle(
-                color: Colors.black,
-              ),
-              decoration: InputDecoration(
-                hintText: 'Enter amount in USD',
-                hintStyle: TextStyle(
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: TextField(
+                style: TextStyle(
                   color: Colors.black,
                 ),
-                prefixIcon: Icon(
-                  Icons.monetization_on_outlined,
-                  color: Colors.black,
+                decoration: InputDecoration(
+                  hintText: 'Enter amount in USD',
+                  hintStyle:  TextStyle(
+                    color: Colors.black,
+                  ),
+                  prefixIcon: Icon(
+                    Icons.monetization_on_outlined,
+                    color: Colors.black,
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: border,
+                  enabledBorder: border,
                 ),
-                filled : true,
-                fillColor: Colors.white,
-
+              
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
               ),
             ),
           ],
